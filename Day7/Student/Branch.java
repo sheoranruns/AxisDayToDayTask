440a847018ae0e1afbbef06d76713666a052a9d6
Priyanshu <64602968+sheoranruns@users.noreply.github.com>
2023-01-11 14:36:18 +0530

    January 5

---
+++ Day7/Student/Branch.java
@@ -0,0 +1,8 @@
+package Student;
+
+public class Branch {
+    String BranchName;
+    int BranchId;
+    String BranchStaff;
+    
+}
---
+++ Day7/Student/Student.java
@@ -0,0 +1,13 @@
+package Student;
+
+public class Student {
+    String StudentName;
+    int StudentId;
+    long StudentPhoneNumber;
+    
+    public Student(String studentName, int studentId, long studentPhoneNumber) {
+        StudentName = studentName;
+        StudentId = studentId;
+        StudentPhoneNumber = studentPhoneNumber;
+    }
+}
